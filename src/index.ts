aa7fdfa8ff952aa91993129950c35498f8c393b5
Dhairya Shah <hello@dhairyashah.dev>
2024-06-08 14:32:26 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+type Env = {
+	AI: any
+
+}
+
+
+const app = new Hono<{ Bindings: Env }>()
+
+app.get("/", async c => {
+	const content = c.req.query('query') || "What is the origin of the phrase Hello World?"
+	const ai = c.env.AI;
+	const messages = [
+		{
+			role: 'system', content: 'You are a friendly assistant.'
+		},
+		{
+			role: 'user', content
+		}
+	]
+	const inputs = { messages }
+
+	const stream = await ai.run("@cf/mistral/mistral-7b-instruct-v0.1", inputs)
+	return c.json(stream);
+
+})
+
+export default app;
